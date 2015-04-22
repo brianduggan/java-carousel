c33b252cb4bb6aaf7c37106800421a9600c1a348
brianduggan <brian.e.duggan@icloud.com>
2015-04-22 19:36:00 -0400

    added js

---
+++ js/carousel.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+	var slideWidth = $('.slide').width(), //the size will dynamically shift based on the CSS, this is JS's purpose
+		numOfSlides = $('.slide').size(),
+		positionNum = 0,
+		slideSpeed = 400;
+
+	// set width of div#tray
+	$('#tray').width(slideWidth * numOfSlides);
+
+	$('a.next').click(function(event){
+		// update the positionNum, calculate slide distance to shift, animate the tray
+		if (positionNum < 2){
+			positionNum += 1;
+		};
+		var distance = positionNum * slideWidth;
+		$('#tray').animate({right:distance}, slideSpeed);
+
+		event.preventDefault();
+	});
+
+	$('a.prev').click(function(event){
+		if (positionNum > 0){
+			positionNum -= 1;
+		};
+		var distance = positionNum * slideWidth;
+		$('#tray').animate({right:distance}, slideSpeed);
+
+		event.preventDefault();
+	});
+
+
+
+});
